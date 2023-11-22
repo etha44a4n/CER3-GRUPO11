591463fa873c92155fdad7f2bd28185c5f6aa0e2
Ethan <ethan.yanez@usm.cl>
2023-11-21 22:06:39 -0300

    Base?

--- CER3/core/models.py
+++ CER3/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Evento(models.Model):
+    TIPO_CHOICES = [
+        ("VA","Vacaciones"),
+        ("FE","Feriado"),
+        ("SS", "Suspensión de actividades"),
+        ("SP", "Suspensión de actividades PM"),
+        ("PL", "Periodo Lectivo"),
+        ("SE", "Suspensión de evaluaciones"),
+        ("CE", "Ceremonia"),
+        ("ED", "EDDA"),
+        ("EV", "Evaluación"),
+        ("AY", "Ayudantías"),
+        ("HA", "Hito Académico"),
+        ("SA", "Secretaría Académica"),
+        ("", "OAI"),
+    ]
+
+    SEGMENTO_CHIOCES = [
+        ("CU", "Comunidad USM"),
+        ("ES", "Estudiante"),
+        ("PR", "Profesor"),
+        ("JF","Jefe de Carrera"),
+    ]
+
+    fechaInicio = models.DateTimeField(auto_now_add=False)
+    fechaTermino = models.DateTimeField(auto_now_add=False)
+    titulo = models.CharField(max_length=55)
+    descripcion = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=2, choices=TIPO_CHOICES)
+    segmento = models.CharField(max_length=2, choices=SEGMENTO_CHIOCES)
--- CER3/core/views.py
+++ CER3/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    title = "Home"
+    data = {
+        "title": title,
+    }
+    return render(request, 'core/home.html', data)
