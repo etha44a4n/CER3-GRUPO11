c3a29051dc610d5e09995f189f03d13ba8f470f4
Nata <natanael.roque@usm.cl>
2023-11-25 15:18:30 -0300

    Cambio en el filtro de tipos

--- CER3/core/views.py
+++ CER3/core/views.py
@@ -5,6 +5,8 @@ def home(request):
     title = "Home"
     segmentos = Evento.SEGMENTO_CHOICES
     segmento_seleccionado = request.GET.get("segmento")
+    tipos = Evento.TIPO_CHOICES
+    tipo_seleccionado = request.GET.get("tipo")
     print(segmento_seleccionado)
     if segmento_seleccionado == 'Todos' or segmento_seleccionado is None:
         eventos = Evento.objects.all()
@@ -16,10 +18,22 @@ def home(request):
                 eventos = Evento.objects.filter(segmento=segmento[0])
                 print(eventos)
                 break
+
+    if tipo_seleccionado == 'Todos' or tipo_seleccionado is None:
+        eventos = Evento.objects.all()
+    else:
+        for tipo in tipos:
+            print(tipo)
+            if str(tipo) == str(tipo_seleccionado):
+                print("Tabn el tipo")
+                eventos = Evento.objects.filter(tipo=tipo[0])
+                print(eventos)
+                break        
         
     data = {
         "title": title,
         "eventos": eventos,
         "segmentos" :segmentos,
+        "tipos": tipos,
     }
     return render(request, 'core/home.html', data)
