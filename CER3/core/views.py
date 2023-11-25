33b7fc9407150cb24d0dffcbff953ca7c0c744bf
Nata <natanael.roque@usm.cl>
2023-11-25 16:03:25 -0300

    Ten ethan

--- CER3/core/views.py
+++ CER3/core/views.py
@@ -7,33 +7,31 @@ def home(request):
     segmento_seleccionado = request.GET.get("segmento")
     tipos = Evento.TIPO_CHOICES
     tipo_seleccionado = request.GET.get("tipo")
-    print(segmento_seleccionado)
+    
     if segmento_seleccionado == 'Todos' or segmento_seleccionado is None:
         eventos = Evento.objects.all()
     else:
         for segmento in segmentos:
-            print(segmento)
             if str(segmento) == str(segmento_seleccionado):
-                print("ola")
                 eventos = Evento.objects.filter(segmento=segmento[0])
-                print(eventos)
                 break
 
     if tipo_seleccionado == 'Todos' or tipo_seleccionado is None:
         eventos = Evento.objects.all()
     else:
         for tipo in tipos:
-            print(tipo)
             if str(tipo) == str(tipo_seleccionado):
                 print("Tabn el tipo")
                 eventos = Evento.objects.filter(tipo=tipo[0])
                 print(eventos)
                 break        
-        
+    print(segmento_seleccionado, tipo_seleccionado)   
     data = {
         "title": title,
         "eventos": eventos,
         "segmentos" :segmentos,
         "tipos": tipos,
+        "segmentoselec": segmento_seleccionado,
+        "tiposelec": tipo_seleccionado,
     }
     return render(request, 'core/home.html', data)
