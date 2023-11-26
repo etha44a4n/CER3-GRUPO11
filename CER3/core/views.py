0421a0a0cd93005816d1fd38ad7f1b4bb9b305ff
Ethan <ethan.yanez@usm.cl>
2023-11-26 20:33:54 -0300

    vistas? no me acuerdo

--- CER3/core/views.py
+++ CER3/core/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Evento, Segmento
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from django.db import IntegrityError
+from django.utils import timezone
 
 def home(request):
     title = "Home"
@@ -11,28 +10,35 @@ def home(request):
     segmento_seleccionado = request.GET.get("segmento")
     tipos = Evento.TIPO_CHOICES
     tipo_seleccionado = request.GET.get("tipo")
-    
+    segmento_usuario = None
+    username = 'Anónimo'
+    eventos_segmento = None
+    if request.user.is_authenticated:
+        username = request.user.username
+        if (str(request.user.tipo_cuenta)== 'Jefe de Carrera' or str(request.user.tipo_cuenta) == 'Profesor'):
+            segmento_usuario = request.user.tipo_cuenta
+            eventos_segmento = Evento.objects.filter(segmento=segmento_usuario, fechaInicio__gte=timezone.now()).order_by('fechaInicio')[:3]
+
     if (segmento_seleccionado == 'Todos' or segmento_seleccionado is None) and (tipo_seleccionado == 'Todos' or tipo_seleccionado is None):
-        eventos = Evento.objects.all()
+        eventos = Evento.objects.filter(fechaInicio__gte=timezone.now()).order_by('fechaInicio')
     else:
         if segmento_seleccionado == 'Todos' or segmento_seleccionado is None:
             for tipo in tipos:
                 if str(tipo[0]) == str(tipo_seleccionado):
-                    eventos = Evento.objects.filter(tipo=tipo[0])
+                    eventos = Evento.objects.filter(tipo=tipo[0], fechaInicio__gte=timezone.now()).order_by('fechaInicio')
                     break
         
         elif tipo_seleccionado == 'Todos' or tipo_seleccionado is None:
             segmento_a_filtrar = Segmento.objects.get(nombre=segmento_seleccionado)
-            eventos = Evento.objects.filter(segmento=segmento_a_filtrar)
+            eventos = Evento.objects.filter(segmento=segmento_a_filtrar, fechaInicio__gte=timezone.now()).order_by('fechaInicio')
 
         else:
             segmento_a_filtrar = Segmento.objects.get(nombre=segmento_seleccionado)
             for tipo in tipos:
                 if tipo[0] == tipo_seleccionado:
-                    eventos = Evento.objects.filter(segmento=segmento_a_filtrar, tipo=tipo[0])
+                    eventos = Evento.objects.filter(segmento=segmento_a_filtrar, tipo=tipo[0], fechaInicio__gte=timezone.now()).order_by('fechaInicio')
                     break
             
-   
     data = {
         "title": title,
         "eventos": eventos,
@@ -40,6 +46,9 @@ def home(request):
         "tipos": tipos,
         "segmento_seleccionado": segmento_seleccionado,
         "tipo_seleccionado": tipo_seleccionado,
+        "segmento_usuario": segmento_usuario,
+        "username": username,
+        "eventos_segmento": eventos_segmento,
     }
     return render(request, 'core/home.html', data)
 
@@ -54,9 +63,7 @@ def iniciarSesion(request):
         }
         return render(request, 'core/login.html', data)
     else:
-        print(authenticate(request, username='percy', password='1234'))
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        print(authenticate(request, username='percy', password='1234'))
         if user is None:
             data = {
                 'form': AuthenticationForm,
