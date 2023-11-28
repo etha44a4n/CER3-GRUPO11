fb35d507cce28c53498e881bde778b861ee0556d
Ethan <ethan.yanez@usm.cl>
2023-11-27 21:17:12 -0300

    modifiqué la bd y el models

--- CER3/core/models.py
+++ CER3/core/models.py
@@ -26,8 +26,8 @@ class Evento(models.Model):
     id = models.BigAutoField(primary_key=True)
     fechaInicio = models.DateTimeField(auto_now_add=False)
     fechaTermino = models.DateTimeField(auto_now_add=False)
-    titulo = models.CharField(max_length=55)
-    descripcion = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
     tipo = models.CharField(max_length=2, choices=TIPO_CHOICES)
     segmento = models.ManyToManyField(Segmento)
 
